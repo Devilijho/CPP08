7f710f2c142082a31f501627294d713841e22e48
Santiago Fuentes <santi.fuentes.llanos@gmail.com>
2025-09-21 12:13:25 +0200

    ex00 done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <vector>
+#include <iostream>
 #include <algorithm>
 #include <exception>
 
 template<typename T>
 int	easyfind(T haystack, int needle)
 {
-	int pos = std::find(haystack.begin(), haystack.end(), needle);
-	if (haystack.end())
+	typename T::iterator it = std::find(haystack.begin(), haystack.end(), needle);
+	if (it == haystack.end())
 		throw std::exception();
-	return pos;
+	return (std::distance(haystack.begin(), it));
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,31 @@
 #include "easyfind.hpp"
-#include <vector>
+#include <exception>
 
 int	main(void)
 {
-	std::vector<int> vector = {1, 4, 6, 9};
-	easyfind(vector, 4);
+	std::vector<int> vector;
+	vector.push_back(1);
+	vector.push_back(4);
+	vector.push_back(6);
+	vector.push_back(9);
+
+	try
+	{
+		std::cout << "Index of value in vector: " << easyfind(vector, 4) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 4 on the vector" << std::endl;
+	}
+
+	try
+	{
+		std::cout << "Index of value in vector: " << easyfind(vector, 3) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 3 on the vector" << std::endl;
+	}
 
 	return (0);
 }
