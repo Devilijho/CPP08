ab2693aca26402cb9c186130689126cf724288da
Santiago Fuentes <santi.fuentes.llanos@gmail.com>
2025-09-25 17:10:10 +0200

    finished ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <iterator>
 #include <list>
 #include <vector>
 #include <iostream>
@@ -13,8 +14,41 @@ class MutantStack : public std::stack<type>
 	public:
 		MutantStack(){}
 		~MutantStack(){}
-		MutantStack(MutantStack &other){(void)other;}
-		MutantStack &operator=(MutantStack &other){(void)other;return *this;}
-		void begin();
-		void end();
+		MutantStack(MutantStack &other) {this = other;}
+		MutantStack &operator=(MutantStack &other)
+		{
+			if (this != other)
+				this = other;
+			return *this;
+		}
+
+		struct iterator
+		{
+			std::forward_iterator_tag Iterator_category;
+			std::ptrdiff_t difference_type;
+			type	*ptr;
+
+			iterator& operator++() { ptr++; return *this;}
+			iterator& operator--() { ptr--; return *this;}
+			type operator*() { return *ptr;}
+			bool operator!=(const iterator& b) { return ptr != b.ptr; };
+		};
+
+		iterator begin()
+		{
+			iterator it;
+
+			it.ptr = &this->top() - this->size() + 1;
+
+			return it;
+		}
+
+		iterator end()
+		{
+			iterator it;
+
+			it.ptr = &this->top() + 1;
+
+			return it;
+		}
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,27 +1,69 @@
 #include "MutantStack.hpp"
+#include <iterator>
+#include <array>
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	// MutantStack<int>::iterator it = mstack.begin();
-	// MutantStack<int>::iterator ite = mstack.end();
-	// ++it;
-	// --it;
-	// while (it != ite)
-	// {
-	// std::cout << *it << std::endl;
-	// ++it;
-	// }
-	// std::stack<int> s(mstack);
-	// return 0;
+	{
+		std::cout << "ITERATOR TEST ON MUTANT STACK" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(50);
+		mstack.push(40);
+		mstack.push(30);
+		mstack.push(20);
+		mstack.push(10);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << "Iterating trough stack: " << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+		std::stack<int> pertinentTessssssst;
+
+		pertinentTessssssst = mstack;
+
+		std::cout << s.size() << std::endl;
+		std::cout << pertinentTessssssst.size() << std::endl;
+		std::cout << "\n\n\n\n";
+	}
+	{
+		std::cout << "ITERATOR TEST ON LIST" << std::endl;
+		std::list<int> list;
+		list.push_back(50);
+		list.push_back(40);
+		list.push_back(30);
+		list.push_back(20);
+		list.push_back(10);
+		list.push_back(0);
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << "Iterating trough list: " << *it << std::endl;
+			++it;
+		}
+		std::cout << "\n\n\n\n";
+	{
+		std::cout << "ITERATOR TEST ON ARRAY" << std::endl;
+		std::array<int, 6> array;
+		array.fill(10);
+		std::array<int, 6>::iterator it = array.begin();
+		std::array<int, 6>::iterator ite = array.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << "Iterating trough array: " << *it << std::endl;
+			++it;
+		}
+	}
+	return 0;
+}
 }
