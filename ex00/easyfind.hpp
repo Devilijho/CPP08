b30537c3ef8c5e55285f50f3bd05d3d59f041c59
Santiago Fuentes Llanos <safuente@f2r2s15.clusters.42paris.fr>
2025-10-31 14:15:24 +0100

    added tests to ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <list>
 #include <exception>
 
 template<typename T>
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,23 +7,80 @@ int	main(void)
 	vector.push_back(4);
 	vector.push_back(6);
 	vector.push_back(9);
+	std::cout << "Created vector with values : 1 4 6 9" << std::endl;
 
 	try
 	{
-		std::cout << "Index of value in vector: " << easyfind(vector, 4) << std::endl;
+		std::cout << "Index of value 4 in vector: " << easyfind(vector, 4) << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << "Didn't find 4 on the vector" << std::endl;
 	}
 
+
 	try
 	{
-		std::cout << "Index of value in vector: " << easyfind(vector, 3) << std::endl;
+		std::cout << "Index of value 6 in vector: " << easyfind(vector, 6) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 6 on the vector" << std::endl;
+	}
+
+	try
+	{
+		std::cout << "Index of value 3 in vector: " << easyfind(vector, 3) << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << "Didn't find 3 on the vector" << std::endl;
+	}
+
+	try
+	{
+		std::cout << "Index of value 1 in vector: " << easyfind(vector, 1) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 1 on the vector" << std::endl;
+	}
+
+	try
+	{
+		std::cout << "Index of value 9 in vector: " << easyfind(vector, 9) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 9 on the vector" << std::endl;
+	}
+
+
+	std::list<int> vectorNotSoVector;
+	vectorNotSoVector.push_back(0);
+	vectorNotSoVector.push_back(2);
+	vectorNotSoVector.push_back(4);
+	vectorNotSoVector.push_back(6);
+	vectorNotSoVector.push_back(8);
+	vectorNotSoVector.push_back(10);
+	vectorNotSoVector.push_back(100);
+	std::cout << "Created list with values : 0 2 4 6 8 10 100" << std::endl;
+
+	try
+	{
+		std::cout << "Index of value 8 in list: " << easyfind(vectorNotSoVector, 8) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 8 on the list" << std::endl;
+	}
+	try
+	{
+		std::cout << "Index of value 9 in list: " << easyfind(vectorNotSoVector, 9) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Didn't find 9 on the list" << std::endl;
 	}
 
 	return (0);
